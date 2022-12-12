877bff565de730effe8a4892512914f2a10faa8c
Steve Landry <steven.m.landry@gmail.com>
2022-12-12 15:09:36 -0700

    Update BuildREport.ps1

--- Intro/18 Report/BuildREport.ps1
+++ Intro/18 Report/BuildREport.ps1
@@ -4,6 +4,7 @@ $locAdmin = Get-LocalGroupMember -Group "Administrators" | Where-Object {$_.Prin
 $uptime = Get-ComputerInfo -Property OsUptime | Select-Object -ExpandProperty OSUpTime
 $psVer = $PSVersionTable | Select-Object -ExpandProperty PSVersion
 
+
 $reportTemplate = New-Object -TypeName psobject
 Add-Member -InputObject $reportTemplate -MemberType NoteProperty -Name OSVersion -Value $null
 Add-Member -InputObject $reportTemplate -MemberType NoteProperty -Name LocalAdmin -Value $null
@@ -11,14 +12,10 @@ Add-Member -InputObject $reportTemplate -MemberType NoteProperty -Name HostName 
 Add-Member -InputObject $reportTemplate -MemberType NoteProperty -Name UPtime -Value $null
 Add-Member -InputObject $reportTemplate -MemberType NoteProperty -Name PSversion -Value $null
 
-$report = New-Object System.Collections.ArrayList
+$report = $reportTemplate | Select-Object *
 
-$tempReport = $reportTemplate | Select-Object *
-
-$tempReport.LocalAdmin = $locAdmin
-$tempReport.OSVersion = $osVer
-$tempReport.HostName = $env:COMPUTERNAME
-$tempReport.UPtime = $uptime
-$tempReport.PSversion = $psVer
-
-$report.add($tempReport)
+$report.LocalAdmin = $locAdmin
+$report.OSVersion = $osVer
+$report.HostName = $env:COMPUTERNAME
+$report.UPtime = $uptime
+$report.PSversion = $psVer
