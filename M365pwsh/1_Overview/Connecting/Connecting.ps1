7a80c096bf64519f3643dd0c6e519f7596a2f916
Steven Landry <steven.m.landry@gmail.com>
2023-03-12 17:26:31 -0400

    20230312-1726

--- M365pwsh/1_Overview/Connecting/Connecting.ps1
+++ M365pwsh/1_Overview/Connecting/Connecting.ps1
@@ -1,22 +1,3 @@
-# https://learn.microsoft.com/en-us/microsoft-365/enterprise/connect-to-microsoft-365-powershell?view=o365-worldwide
-
-# being deprecated in June 2023
-Install-Module MSOnline
-Install-Module  AzureAD 
-
-Import-Module -Name azuread
-Import-Module -Name MSOnline
-
-Connect-AzureAD
-Connect-MsolService
-
-
-# newest modules
-Install-Module Microsoft.Graph -Scope AllUsers
-
--- Dont import the modules --
-#Import-Module -Name Microsoft.Graph
-
 $params = @(
     'User.ReadWrite.All',
     "Group.ReadWrite.All", 
@@ -28,7 +9,8 @@ $params = @(
     "TeamSettings.ReadWrite.All",
     "Files.ReadWrite.All",
     "ReportSettings.ReadWrite.All",
-    "Reports.Read.All"
+    "Reports.Read.All",
+    "Sites.FullControl.All"
 )
 
 Connect-MgGraph -Scopes $params
--- M365pwsh/OneDrive/scratch.ps1
+++ M365pwsh/OneDrive/scratch.ps1
@@ -1,1 +1,6 @@
 Install-Module SharePointPnPPowerShellOnline -Force
+
+Import-Module SharePointPnPPowerShellOnline
+
+Uninstall-Module -Name SharePointPnPPowerShellOnline -AllVersions -Force
+Install-Module -Name PnP.PowerShell
